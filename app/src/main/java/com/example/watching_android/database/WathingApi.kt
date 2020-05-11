c2247b82245d1305f79c33d7274aec15a7685d48
kyuuki <kyuuki.japan@gmail.com>
2020-05-11 15:20:30 +0900

    Output Retrofit communication log

--- app/src/main/java/com/example/watching_android/database/WathingApi.kt
+++ app/src/main/java/com/example/watching_android/database/WathingApi.kt
@@ -1,6 +1,8 @@
 package com.example.watching_android.database
 
 import com.example.watching_android.constants.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -12,9 +14,18 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 object WatchingApi {
     val client: JsonPlaceHolder by lazy {
+        // 通信ログ出力
+        // https://tech.mti.co.jp/entry/2020/03/31/163321
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        // 通信ログ出力のためにデフォルト OkHttpClient 以外を利用する
+        val httpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         val retrofit = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(Constants.baseUrl)
+            .client(httpClient)
             .build()
 
         retrofit.create(JsonPlaceHolder::class.java)
